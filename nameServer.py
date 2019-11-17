97914113217f7a146dfb5ff82b73ae2ff7437e4d
Daniel <danielperezcascon@gmail.com>
2019-11-17 16:43:14 +0000

    Error managing for issues with

--- nameServer.py
+++ nameServer.py
@@ -3,50 +3,68 @@ from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__, static_url_path="")
 
-figure_names_men = ['Albert Einstein', 'Abraham Lincoln', 'Franklin D. Roosevelt', 'Winston Churchill',
-                    'George Washington', 'Mansa Musa', 'Mao Zedong']
-figure_names_women = ['Queen Victoria', 'Jane Austen', 'Rosa Parks', 'Catherine the Great', 'Malala Yousafzai',
-                      'Marie Curie', 'Rosalind Franklin', 'Margaret Thatcher']
+continents = ['europe', 'asia', 'africa', 'north america', 'south america']
+
+male_figures = [{'name': 'Albert Einstein', 'gender': 'male', 'continent': 'europe'},
+                {'name': 'Winston Churchill', 'gender': 'male', 'continent': 'europe'},
+                {'name': 'Mansa Musa', 'gender': 'male', 'continent': 'africa'},
+                {'name': 'Haile Selassie', 'gender': 'male', 'continent': 'africa'},
+                {'name': 'Mao Zedong', 'gender': 'male', 'continent': 'asia'},
+                {'name': 'Saddam Hussein', 'gender': 'male', 'continent': 'asia'},
+                {'name': 'Abraham Lincoln', 'gender': 'male', 'continent': 'north america'},
+                {'name': 'Mark Twain', 'gender': 'male', 'continent': 'north america'},
+                {'name': 'Che Guevara', 'gender': 'male', 'continent': 'south america'},
+                {'name': 'Simon Bolivar', 'gender': 'male', 'continent': 'south america'}]
+
+female_figures = [{'name': 'Alfonsina Storni', 'gender': 'female', 'continent': 'south america'},
+                  {'name': 'Isabel Allende', 'gender': 'female', 'continent': 'south america'},
+                  {'name': 'Marlyn Monroe', 'gender': 'female', 'continent': 'north america'},
+                  {'name': 'Rosa Parks', 'gender': 'female', 'continent': 'north america'},
+                  {'name': 'Wu Zetian', 'gender': 'female', 'continent': 'asia'},
+                  {'name': 'Malala Yousafzai', 'gender': 'female', 'continent': 'asia'},
+                  {'name': 'Yennenga', 'gender': 'female', 'continent': 'africa'},
+                  {'name': 'Miriam Makeba', 'gender': 'female', 'continent': 'africa'},
+                  {'name': 'Queen Victoria', 'gender': 'female', 'continent': 'europe'},
+                  {'name': 'Catherine the Great', 'gender': 'female', 'continent': 'europe'}, ]
 
 
-@app.route('/figure/<string:gender>', methods=['GET'])
+@app.route('/figure/<string:gender>/<continent>/', methods=['GET'])
+@app.route('/figure/<string:gender>/', methods=['GET'])
 @app.route('/figure/', methods=['GET'])
-def get_person(gender=None):
+def get_person(gender=None, continent=None):
+
     if gender == 'male':
-        return jsonify({'name': random.choice(figure_names_men), 'gender': 'male'})
+        temp = []
+        if continent and continent in continents:
+            for i in male_figures:
+                if i['continent'] == continent:
+                    temp.append(i)
+        elif continent and continent not in continents:
+            return 'Unrecognised parameter', 404
+        else:
+            temp = male_figures.copy()
+        return jsonify(random.choice(temp))
+
     elif gender == 'female':
-        return jsonify({'name': random.choice(figure_names_women), 'gender': 'female'})
+        temp = []
+        if continent and continent in continents:
+            for i in female_figures:
+                if i['continent'] == continent:
+                    temp.append(i)
+        elif continent and continent not in continents:
+            return 'Unrecognised parameter', 404
+        else:
+            temp = female_figures.copy()
+        return jsonify(random.choice(temp))
     elif not gender:
-        val = random.choice(figure_names_men + figure_names_women)
-        return jsonify({'name': val, 'gender': ['male' if figure_names_women.count(val) == 0 else 'female']})
+        return jsonify(random.choice(male_figures+female_figures))
     else:
-        abort(404, 'Unrecognised parameter')
-
-
-@app.route('/figure/', methods=['POST'])
-def post_figure():
-    if request.json:
-        if request.json['gender'] == 'female':
-
-            if figure_names_women.count(request.json['name']) != 0:
-                abort(403, 'Already contained')
-            figure_names_women.append(request.json['name'])
-
-        elif request.json['gender'] == 'male':
-
-            if figure_names_men.count(request.json['name']) != 0:
-                abort(403, 'Already contained')
-            figure_names_men.append(request.json['name'])
-
-    return 'Appended', 201
+        return 'Unrecognised parameter', 404
 
 
 @app.route('/all_figures/', methods=['GET'])
 def get_list():
-    all_men_json = [{'name': item, 'gender': 'male'} for item in figure_names_men]
-    all_women_json = [{'name': item, 'gender': 'female'} for item in figure_names_women]
-    all_people = all_men_json + all_women_json
-    return jsonify({'all_figures': all_people})
+    return jsonify(male_figures+female_figures)
 
 
 if __name__ == '__main__':
--- webClient/__init__.py
+++ webClient/__init__.py
@@ -1,34 +1,42 @@
-import json
-
 from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
-my_rest_URL = 'http://localhost:9999/'
-my_rest_figures = 'figure/'
+REST_FIGURES = 'http://localhost:9999/figure'
 
 
 @app.route("/")
 def hello():
-    return render_template('base.html', output="", item=False)
+    return render_template('base.html', output="", item=False, error=False, msg="")
 
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     # text = request.form['inputText']
-
+    query = REST_FIGURES
     if request.form['submit_button'] == 'clear':
-        return render_template('base.html', output="", item=False)
+        return render_template('base.html', output="", item=False, error=False, msg="")
     else:
         if 'gender' in request.form:
             gender = request.form['gender']
-            req = requests.get(my_rest_URL + my_rest_figures + gender)
-        else:
-            req = requests.get(my_rest_URL + my_rest_figures)
+            query += gender + '/'
 
-        res = req.json()['name']
-        return render_template('base.html', output=res, item=True)
+        if request.form['continent'] != "":
+            continent = request.form['continent']
+            query +=  continent.lower() + '/'
+
+        req = requests.get(query)
+
+        if 'name' in str(req.content) and 'continent' in str(req.content):
+            name = req.json()['name']
+            continent = req.json()['continent']
+        else:
+            return render_template('base.html', name="", continent="", item=False,
+                                   error=True, msg=req.reason)
+
+        return render_template('base.html', name=name, continent=continent, item=True,
+                               error=False, msg="")
 
 
 if __name__ == "__main__":
