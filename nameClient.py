9e0f4f6bf819eafd664586199f94faf9e957c90b
Daniel Perez Cascon <sc17dpc@leeds.ac.uk>
2019-11-18 11:40:23 +0000

    added changes

--- nameClient.py
+++ nameClient.py
@@ -6,6 +6,18 @@ female_path = 'figure/female'
 random_path = 'figure/'
 all_path = 'all_figures/'
 
+REST_BOOKS = 'https://www.goodreads.com/search/index.xml'
+GOOD_READS_KEY = 'kasyZkeIPzsma2EBH0Lu9w'
+
+query = "Test"
+payload = {'q': query, 'key': GOOD_READS_KEY}
+resp = requests.get(REST_BOOKS, params=payload)
+print(resp.text)
+
+REST_TODAY = 'http://history.muffinlabs.com/date'
+resp = requests.get(REST_TODAY)
+print(resp.json())
+
 print(requests.get(URL + male_path).json())
 print(requests.get(URL + female_path).json())
 print(requests.get(URL + random_path).json())
